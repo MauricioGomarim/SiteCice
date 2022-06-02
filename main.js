7f8cc04239fd35190c39f39d8be4752eca134494
mauricio <mauricio.gomarim3@gmail.com>
2022-06-01 18:52:57 -0700

    menu

--- main.js
+++ main.js
@@ -18,14 +18,18 @@ menu.addEventListener('click', menuhide)
 
 const menuBtn = document.querySelector('.menu-btn')
 
+const body = document.querySelector('body')
+
 let menuOpen = false
 
 menuBtn.addEventListener('click', () => {
   if (!menuOpen) {
     menuBtn.classList.add('open')
+    body.classList.add('hide')
     menuOpen = true
   } else {
     menuBtn.classList.remove('open')
+    body.classList.remove('hide')
     menuOpen = false
   }
 
